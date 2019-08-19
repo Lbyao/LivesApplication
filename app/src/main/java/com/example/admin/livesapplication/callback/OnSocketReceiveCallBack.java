df775f96b7f860446c4fd7b658e5f52cacd09239
lmr <1483587277@qq.com>
2019-08-19 15:40:21 +0800

    增加注释

--- app/src/main/java/com/example/admin/livesapplication/callback/OnSocketReceiveCallBack.java
+++
@@ -1,8 +0,0 @@
-package com.example.admin.livesapplication.callback;
-
-/**
- * Created by Administrator on 2018/5/7.
- */
-public interface OnSocketReceiveCallBack {
-    void receiveMsg(String msg);
-}
